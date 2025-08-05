5dc729b16495ef7e36783bcec251116c14774481
Kascabrel <107616396+Kascabrel@users.noreply.github.com>
2025-08-05 13:42:52 +0200

    Bind the Database with the App

--- services/auth_service/app/api/auth_endpoints.py
+++ services/auth_service/app/api/auth_endpoints.py
@@ -1,10 +1,21 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends
 from fastapi.security import OAuth2PasswordBearer
+from sqlalchemy.orm import Session
+
+from db import sessionLocal
 
 router = APIRouter()
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="login")
 
 
+def get_db():
+    db = sessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+
 @router.post("/home")
-def hello_world():
+def hello_world(db: Session = Depends(get_db)):
     return {"hello": "world"}
--- services/auth_service/app/main.py
+++ services/auth_service/app/main.py
@@ -1,7 +1,18 @@
+from contextlib import asynccontextmanager
+
 from fastapi import FastAPI
 
 from api.auth_endpoints import router
+from db import Base, engine
 
-app = FastAPI(title="Authservice")
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    # 💡create the table by start
+    Base.metadata.create_all(bind=engine)
+    yield  # server start here
+
+
+app = FastAPI(title="Authservice", lifespan=lifespan)
 
 app.include_router(router)
