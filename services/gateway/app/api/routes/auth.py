106d9c59f6490446a2f0967dac5dc6ca7dd99dc8
Kascabrel <107616396+Kascabrel@users.noreply.github.com>
2025-10-03 18:18:05 +0200

    Add:  response model to the gateway's endpoint

--- services/gateway/app/api/routes/auth.py
+++ services/gateway/app/api/routes/auth.py
@@ -4,13 +4,6 @@ from fastapi import APIRouter, Request
 
 router = APIRouter()
 
-from shared.schemas.user import UserCreate, UserOut
-from shared.schemas.token import (
-    RefreshTokenRequest,
-    RefreshTokenResponse,
-    LoginResponse,
-    TokenData,
-)
 
 
 @router.get("/home")
@@ -20,28 +13,28 @@ async def home():
         return response.json()
 
 
-@router.post("/login", response_model=LoginResponse)
+@router.post("/login")
 async def login(request: Request):
     async with httpx.AsyncClient(base_url="http://auth-service:8000") as client:
         response = await client.post("/auth/login", data=await request.form())
         return response.json()
 
 
-@router.post("/register", response_model=UserOut)
-async def register(user_create: UserCreate, request: Request):
+@router.post("/register")
+async def register(request: Request):
     async with httpx.AsyncClient(base_url="http://auth-service:8000") as client:
         response = await client.post("/auth/register", json=await request.json())
         return response.json()
 
 
-@router.post("/refresh-token", response_model=RefreshTokenResponse)
+@router.post("/refresh-token")
 async def refresh_token(request: Request):
     async with httpx.AsyncClient(base_url="http://auth-service:8000") as client:
         response = await client.post("/auth/refresh-token", json=await request.json())
         return response.json()
 
 
-@router.get("/me",  response_model=UserOut)
+@router.get("/me")
 async def get_me(authorization: str = Header(...)):
     headers = {"Authorization": authorization}
     async with httpx.AsyncClient(base_url="http://auth-service:8000") as client:
